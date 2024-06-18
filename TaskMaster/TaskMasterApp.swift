5772d06212ae6c1266dd98d54dda3d88de3c2949
Brian Farkas <brianfarkas75@gmail.com>
2024-06-18 04:02:46 -0500

    Initial Commit

---
+++ TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by Brian Farkas on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
