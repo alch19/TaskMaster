f8d4b71c11e0f9d27a1fd69fc8b630fedb903f2f
Brian Farkas <brianfarkas75@gmail.com>
2024-06-18 16:47:38 -0500

    Implementing Compile To Do

---
+++ TaskMaster/TasksViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TasksViewModel.swift
+//  TaskMaster
+//
+//  Created by Brian Farkas on 6/18/24.
+//
+
+import SwiftUI
+
+struct TasksViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TasksViewModel()
+}
